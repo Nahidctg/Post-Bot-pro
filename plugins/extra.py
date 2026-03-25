a3ea1829dd4b5240f54879e3b75f37c06f3b1226
Nahidctg <164381234+Nahidctg@users.noreply.github.com>
2026-03-25 12:31:48 +0600

    Delete plugins directory

--- plugins/extra.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("test"))
-async def test_plugin(client, message):
-    await message.reply_text("প্লাগিন সিস্টেম সঠিকভাবে কাজ করছে!")
